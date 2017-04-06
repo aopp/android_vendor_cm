6ef1ae204ee4bb3068e686a3c6f9d111c0d4dba5
Timothy Mossey <tim@pwnieexpress.com>
2017-04-06 13:28:35 -0400

    Change gocd to jenkins

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s https://raw.githubusercontent.com/aopp/gocd/master/aopp-build-targets | sed -e 's/#.*$//' | grep px-0.1 | awk '{printf "aopp_%s-%s\n", $1, $2}')
+for combo in $(curl -s https://raw.githubusercontent.com/aopp/jenkins/master/aopp-build-targets | sed -e 's/#.*$//' | grep px-0.1 | awk '{printf "aopp_%s-%s\n", $1, $2}')
 do
     add_lunch_combo $combo
 done
